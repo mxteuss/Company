66aa789506c6a6cd56c8ecc4aeca718e65bc6d1d
mxteuss <ovagabundant@gmail.com>
2025-09-25 17:45:23 -0300

    chore: Migrando o projeto para Spring Boot na camada Service

--- main/java/com/company/employees/service/employeeService.java
+++ main/java/com/company/employees/service/employeeService.java
@@ -1,263 +1,55 @@
-package service;
+package com.company.employees.service;
 
-import dao.connectionFactory;
-import lombok.SneakyThrows;
+
+import com.company.employees.repository.EmployeeRepository;
 import lombok.extern.log4j.Log4j2;
-import model.Employee;
-import model.Enum.Tipo;
-import org.apache.logging.log4j.core.appender.mom.jeromq.JeroMqManager;
-import util.Singleton;
+import com.company.employees.model.Employee;
+import org.springframework.stereotype.Service;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
-import static java.sql.Types.NULL;
+import java.util.Optional;
 
 @Log4j2
+@Service
 public class employeeService {
-    private static Connection conn = null;
 
-    public employeeService(Connection conn) {
-        this.conn = conn;
+    public EmployeeRepository repository;
+
+    public employeeService(EmployeeRepository repository) {
+        this.repository = repository;
     }
 
-    public static void salvar(Employee emp) {
-        String sql = "INSERT INTO Employees (name, type, salary, variable) VALUES (?, ?, ?, ?)";
-        try (Connection conn = connectionFactory.getConnection()) {
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, emp.getNome());
-            stmt.setString(2, String.valueOf(emp.getTipo()));
-            stmt.setInt(3, emp.getSalarioBase());
-            if (String.valueOf(emp.getTipo()).equalsIgnoreCase("Efetivo")) {
-                stmt.setInt(4, NULL); //Inserindo NULL na tabela
-            } else {
-                stmt.setDouble(4, emp.getAdicionalVar());
-            }
-            int rows = stmt.executeUpdate();
-            stmt.close();
-            log.info("Linhas afetadas: " + rows);
-        } catch (SQLException e) {
-            log.error("Erro: " + e.getMessage());
-        }
+    public Employee salvar(Employee emp) {
+        return repository.save(emp);
     }
 
-    public static void delete() {
-        String sql = "DELETE FROM `Company`.`Employees` WHERE (`id` = ?)";
-        try (Connection conn = connectionFactory.getConnection()) {
-            log.info("Insira o ID do funcionário a ser deletado: ");
-            Scanner sc = Singleton.getInstance();
-            int id = sc.nextInt();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setInt(1, id);
-            int rows = stmt.executeUpdate();
-            if (rows > 0) {
-                log.info("Funcionário com ID '{}' foi deletado. Linhas afetadas: {}", id, rows);
-            } else {
-                log.warn("Nenhum funcionário com o ID '{}' foi encontrado no banco de dados.", id);
-            }
-        } catch (SQLException e) {
-            log.error("Erro ao deletar dados: " + e.getMessage());
-        }
+    public String delete(Employee emp) {
+        repository.delete(emp);
+        return "Usuário: " + emp.getId() + "deletado";
     }
 
-    public static void uptade() {
-        String sql = "UPDATE `Company`.`Employees` SET `type` = ?, `salary` = ?, `variable` = ? WHERE `id` = ?";
-        Scanner sc = Singleton.getInstance();
-        try (Connection conn = connectionFactory.getConnection()) {
-            log.info("Insira o id do funcionário à ter os dados atualizados: ");
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            int id = sc.nextInt();
-            stmt.setInt(4, id);
-            sc.nextLine();
-            log.info("Insira o tipo contratual do funcionário: ");
-
-            String type = sc.nextLine();
-
-
-            if (type.equalsIgnoreCase("Efetivo")) {
-                stmt.setInt(3, NULL);
-                stmt.setString(1, type.toUpperCase());
-                sc.nextLine();
-
-                // calculando salário final do efetivado
-                log.info("Insira o salário do funcionário: ");
-                int salary = sc.nextInt();
-                salary += 500;
-                stmt.setInt(2, salary);
-            }
-            if (type.equalsIgnoreCase("Terceirizado")){
-                stmt.setString(1, type.toUpperCase());
-                sc.nextLine();
-                log.info("Insira o salário do funcionário: ");
-                int salary = sc.nextInt();
-                stmt.setInt(2, salary);
-
-                log.info("Insira a variável adicional do funcionário: ");
-                int var = sc.nextInt();
-                stmt.setInt(3, var);
-            }
-            int rows = stmt.executeUpdate();
-            if (rows > 0) {
-                log.info("Funcionário com ID '{}' foi atualizado. Linhas afetadas: {}", id, rows);
-            } else {
-                log.warn("Nenhum funcionário com o ID '{}' foi encontrado no banco de dados.", id);
-            }
-            stmt.close();
-
-        } catch (SQLException e) {
-            log.error("Erro ao atualizar dados: " + e.getMessage());
+    public Employee uptade(Employee emp) {
+        Employee novoEmp = new Employee();
+        emp = novoEmp;
+        if (emp.getTipo().getDesc().equalsIgnoreCase("Efetivo")){
+            emp.setAdicional(null);
         }
+        return repository.save(novoEmp);
     }
 
-    public static StringBuilder findAll() {
-        log.info("Procurando todos  os funcionários");
-        String sql = "SELECT * FROM Company.Employees";
-        List<Employee> employees = new ArrayList<>();
-        String string = null;
-        try (Connection conn = connectionFactory.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)
-        ) {
-            while (rs.next()) {
-                employees.add(
-                        Employee.builder()
-                                .id(rs.getInt("id"))
-                                .tipo(Tipo.valueOf(rs.getString("type")))
-                                .nome(rs.getString("name"))
-                                .salarioBase(rs.getInt("salary"))
-                                .adicionalVar(rs.getInt("variable"))
-                                .build());
-            }
 
-
-        } catch (SQLException e) {
-            log.error("Erro na pesquisa dos funcionários: " + e.getMessage());
-        }
-             StringBuilder stringBuilder = new StringBuilder();
-        for (Employee employee : employees) {
-            stringBuilder.append("Id: ");
-            stringBuilder.append(employee.getId());
-            stringBuilder.append("\n");
-            stringBuilder.append("Nome: ");
-            stringBuilder.append(employee.getNome());
-            stringBuilder.append("\n");
-            stringBuilder.append("Tipo contratual: ");
-            stringBuilder.append(employee.getTipo());
-            stringBuilder.append("\n");
-
-            stringBuilder.append("Salário base: ");
-            stringBuilder.append(employee.getSalarioBase());
-            stringBuilder.append("\n");
-            stringBuilder.append("Adicional variável: ");
-            stringBuilder.append(employee.getAdicionalVar() != null ? employee.getAdicionalVar() : "Não informado");
-            stringBuilder.append("\n");
-            stringBuilder.append("------------------------");
-            stringBuilder.append("\n");
-
-
-
-
-        }
-        return stringBuilder;
+    public List<Employee> findAll() {
+    return repository.findAll();
     }
 
-    @SneakyThrows
-    public static StringBuilder search() {
-        log.info("Procurando funcionário...");
-        Scanner sc = Singleton.getInstance();
-        String sql = "SELECT * FROM Company.Employees WHERE name = ?";
-        List<Employee> employees = new ArrayList<>();
-
-        log.info("Insira o  nome do funcionário: ");
-        String s = sc.nextLine();
-
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, s);
-
-            try (ResultSet rs = ps.executeQuery()) {
-
-                while (rs.next()) {
-                    String tipoStr = rs.getString("type");
-
-                    Tipo tipo = Tipo.valueOf(tipoStr);
-                    employees.add(
-                            Employee.builder()
-                                    .id(rs.getInt("id"))
-                                    .nome(rs.getString("name"))
-                                    .tipo(tipo)
-                                    .salarioBase(rs.getInt("salary"))
-                                    .adicionalVar(rs.getInt("variable"))
-                                    .build());
-
-                }
-
-            } catch (SQLException e) {
-                log.error("Erro na pesquisa dos funcionários: " + e.getMessage());
-            }
-            StringBuilder stringBuilder = new StringBuilder();
-            for (Employee employee : employees) {
-                stringBuilder.append("Id: ");
-                stringBuilder.append(employee.getId());
-                stringBuilder.append("\n");
-                stringBuilder.append("Nome: ");
-                stringBuilder.append(employee.getNome());
-                stringBuilder.append("\n");
-                stringBuilder.append("Tipo contratual: ");
-                stringBuilder.append(employee.getTipo());
-                stringBuilder.append("\n");
-
-                stringBuilder.append("Salário base: ");
-                stringBuilder.append(employee.getSalarioBase());
-                stringBuilder.append("\n");
-                stringBuilder.append("Adicional variável: ");
-                stringBuilder.append(employee.getAdicionalVar() != null ? employee.getAdicionalVar() : "Não informado");
-                stringBuilder.append("\n");
-                stringBuilder.append("------------------------");
-                stringBuilder.append("\n");
-
-
-
-
-            }
-            return stringBuilder;
-        }
-
-
+    public Optional<Employee> findById(Employee emp) {
+    return repository.findById(emp.getId());
     }
 
-    public static List<Integer> report(){
-        List<Integer> avg = new ArrayList<>();
-        String sql = "SELECT salary, variable FROM `Company`.`Employees`";
-        int sum = 0;
-        int i = 0;
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)){
-
-            try(ResultSet rs = ps.executeQuery()){
-            while (rs.next()){
-                avg.add(rs.getInt("salary"));
-                avg.add(rs.getInt("variable"));
-
-                for (int total : avg){
-                    sum += total;
-                }
-                i = sum / avg.size();
-            }
-            }
-        }
-        catch (SQLException e){
-            log.error("Erro ao gerar relatórios: " + e.getMessage());
-        }
-        log.info("Total: " + sum);
-        log.info("Média: " + i);
-
-        return avg;
-    }
-
-    }
+//    public static List<Integer> report() {
+//    Deixado de lado por enquanto
+//    }
+}
 
 
 
