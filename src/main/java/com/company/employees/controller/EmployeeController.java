70fa9e6769ad354bfc09959835d85b6548fabe2e
mxteuss <ovagabundant@gmail.com>
2025-09-26 15:16:21 -0300

    feat: Endpoint para mostrar funcionários

--- src/main/java/com/company/employees/controller/EmployeeController.java
+++ src/main/java/com/company/employees/controller/EmployeeController.java
@@ -1,12 +1,24 @@
 package com.company.employees.controller;
 
+import com.company.employees.model.Employee;
 import com.company.employees.service.EmployeeService;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+
+@RestController
 @AllArgsConstructor
 public class EmployeeController {
 
     public EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public ResponseEntity<Page<Employee>> findAll(){
+        Page<Employee> employees = employeeService.findAll(0, 10);
+        return ResponseEntity.ok(employees);
+    }
 }
