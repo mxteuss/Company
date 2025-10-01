837cfe19a3eef319aa2e0eeb81070f22e0d6c801
mxteuss <ovagabundant@gmail.com>
2025-09-30 22:58:21 -0300

    feat: add entidade User para autenticação

---
+++ src/main/java/com/company/employees/domain/auth/User.java
@@ -0,0 +1,49 @@
+package com.company.employees.domain.auth;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Table(name = "users")
+@Entity
+@Data
+public class User implements UserDetails {
+    private String nome;
+    private String login;
+    private String password;
+    private Roles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
