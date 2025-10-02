9d65ac6235d9d6b83e5dd7c75fe896a3ce583b67
mxteuss <ovagabundant@gmail.com>
2025-10-01 23:05:08 -0300

    feat: add repositório de User

---
+++ src/main/java/com/company/employees/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.company.employees.repository;
+
+import com.company.employees.domain.auth.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
