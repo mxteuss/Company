fdae9f8ee9c165dd4d4331f8e4d1e501784c4357
mxteuss <ovagabundant@gmail.com>
2025-10-12 01:48:01 -0300

    feat: add RepresentationModel

---
+++ src/main/java/com/company/employees/representation/EmployeeRepresentation.java
@@ -0,0 +1,17 @@
+package com.company.employees.representation;
+
+import com.company.employees.domain.model.Enum.Tipo;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+@Getter
+@Setter
+public class EmployeeRepresentation extends RepresentationModel<EmployeeRepresentation> {
+    private Long id;
+    private Tipo tipo;
+    private String nome;
+    private Integer salario;
+    private Integer adicional;
+
+}
