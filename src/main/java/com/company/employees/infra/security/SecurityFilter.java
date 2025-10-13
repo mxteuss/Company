0f61264cdff24a9d23e6ba1fc23f548a792ba46c
mxteuss <ovagabundant@gmail.com>
2025-10-12 23:34:43 -0300

    fix: Validação de token do usuário

--- src/main/java/com/company/employees/infra/security/SecurityFilter.java
+++ src/main/java/com/company/employees/infra/security/SecurityFilter.java
@@ -1,11 +1,11 @@
 package com.company.employees.infra.security;
 
+
 import com.company.employees.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.antlr.v4.runtime.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,28 +16,31 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-
 public class SecurityFilter extends OncePerRequestFilter {
-
     @Autowired
-    private TokenService tokenService;
-    private UserRepository userRepository;
+    TokenService tokenService;
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if (token != null) {
+        if(token != null) {
             var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByLogin(login);
-
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if (login != null) {
+                UserDetails user = userRepository.findByLogin(login);
+            if (user != null) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            }
         }
         filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
-        if (authHeader == null)return null;
-        return authHeader.replace("Barear", "");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
     }
 }
