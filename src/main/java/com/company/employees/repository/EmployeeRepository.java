7408513bd6117af0988a2665a490ca8580e182d0
mxteuss <ovagabundant@gmail.com>
2025-10-12 23:35:26 -0300

    refactor: alterando pacote de importação

--- src/main/java/com/company/employees/repository/EmployeeRepository.java
+++ src/main/java/com/company/employees/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
 package com.company.employees.repository;
 
-import com.company.employees.domain.model.Employee;
+import com.company.employees.domain.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
