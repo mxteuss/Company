712bb5d364f4f9883d95d50546c949ffac0b4b15
mxteuss <ovagabundant@gmail.com>
2026-01-02 18:47:59 -0300

    feat: Método para gerar relatório

--- EmployeeManager/src/main/java/com/company/employees/service/EmployeeService.java
+++ EmployeeManager/src/main/java/com/company/employees/service/EmployeeService.java
@@ -6,18 +6,23 @@ import com.company.employees.models.employee.Employee;
 import com.company.employees.producer.EmployeeProducer;
 import com.company.employees.repository.EmployeeRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import lombok.NonNull;
+import org.openpdf.text.Document;
+import org.openpdf.text.Font;
+import org.openpdf.text.Paragraph;
+import org.openpdf.text.pdf.PdfPTable;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 
 @Service
@@ -27,9 +32,9 @@ public class EmployeeService {
     private String emailFrom;
 
 
-    private EmployeeRepository repository;
-    private EmployeeProducer employeeProducer;
-    private JavaMailSender mailSender;
+    private final EmployeeRepository repository;
+    private final EmployeeProducer employeeProducer;
+    private final JavaMailSender mailSender;
 
     public EmployeeService(EmployeeRepository repository, EmployeeProducer employeeProducer, JavaMailSender mailSender) {
         this.repository = repository;
@@ -39,58 +44,83 @@ public class EmployeeService {
 
     @Transactional
     public Employee save(Employee emp) {
-        if(emp.getTipo().equals(Tipo.Efetivo)){
+        if (emp.getTipo().equals(Tipo.Efetivo)) {
             emp.setAdicional(null);
         }
         employeeProducer.sendEmail(emp);
         return repository.save(emp);
     }
 
-    public String deleteById(Long id) {
-        repository.deleteById(id);
-        return "Usuário: " + id + "deletado";
+    public void deleteById(Long id) {
+         repository.deleteById(id);
     }
 
     public Employee uptade(Employee emp) {
         Employee novoEmp = new Employee();
         emp = novoEmp;
-        if (emp.getTipo().equals(Tipo.Efetivo)){
+        if (emp.getTipo().equals(Tipo.Efetivo)) {
             emp.setAdicional(null);
         }
         return repository.save(novoEmp);
     }
 
 
-    public Page<Employee> findAll(int pageNO, int pageSize) {
+    public Page<@NonNull Employee> findAll(int pageNO, int pageSize) {
         Pageable pageable = PageRequest.of(pageNO, pageSize);
         return repository.findAll(pageable);
 
     }
 
     public Optional<Employee> findById(Long id) {
-    return Optional.ofNullable(repository.findById(id)
-            .orElseThrow(() -> new RuntimeException("Usuário não encontrado")));
+        return Optional.of(repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado")));
     }
 
 
-    public String report() {
+    public byte[] report() {
+        Document document = new Document();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        document.open();
+
+        Font font = new Font(Font.HELVETICA, 18, Font.NORMAL);
+        Paragraph titulo = new Paragraph("Relatório", font);
+        titulo.setAlignment(Paragraph.ALIGN_CENTER);
+        titulo.setSpacingAfter(20);
+        titulo.setSpacingBefore(20);
+        document.add(titulo);
+
+        PdfPTable table = new PdfPTable(4);
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(20);
+        table.setSpacingAfter(20);
+
+        table.addCell("Funcionário ");
+        table.addCell("Contrato");
+        table.addCell("Salário");
+        table.addCell("E-mail");
+
         List<Employee> employees = repository.findAll();
 
-    double sum = 0;
-    for (Employee e : employees) {
-        if (e.getAdicional() == null) {
-            sum += e.getSalario();
-        } else {
-            sum += (e.getSalario() + e.getAdicional());
+        for (Employee e : employees) {
+            table.addCell(e.getNome());
+            table.addCell(String.valueOf(e.getTipo()));
+            table.addCell(String.valueOf(e.getSalario()));
+            table.addCell(e.getEmail());
         }
-    }
-    double media = sum / employees.size();
-    return "Total: " + sum
-            + "\nMédia: " + media;
+        document.add(table);
+        document.close();
+
+        mailSender.send(message -> {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
+            message.setFrom(emailFrom);
+            message.setSubject("Relatório de Funcionários");
+            message.setText("Segue em anexo o relatório de funcionários.");
+            helper.addAttachment("relatorio_funcionarios.pdf", new ByteArrayResource(baos.toByteArray()));
+        });
+        return baos.toByteArray();
     }
 }
-
-
 
 
 
