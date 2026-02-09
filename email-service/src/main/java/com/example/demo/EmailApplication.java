a7400303f64f6b012508f2dc359635a82e4d4469
mxteuss <ovagabundant@gmail.com>
2026-02-09 18:13:03 -0300

    chore: classe main

--- email-service/src/main/java/com/example/demo/EmailApplication.java
+++ email-service/src/main/java/com/example/demo/EmailApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableRabbit
-public class DemoApplication {
+public class EmailApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(EmailApplication.class, args);
     }
 
 }
