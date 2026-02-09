b765f19e46989de937eb4e1912d09db190108107
mxteuss <ovagabundant@gmail.com>
2026-02-09 18:14:22 -0300

    feat: Documentação de API

--- EmployeeManager/src/main/java/com/company/employees/controller/UserController.java
+++ EmployeeManager/src/main/java/com/company/employees/controller/UserController.java
@@ -5,6 +5,8 @@ import com.company.employees.models.auth.RegisterDTO;
 import com.company.employees.models.auth.User;
 import com.company.employees.repository.UserRepository;
 import com.company.employees.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +16,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
+@Tag(name = "authentication", description = "Endpoints para autenticação e registro de usuários")
 public class UserController {
     public UserRepository userRepository;
     public AuthService authService;
 
+
+    @Operation(summary = "User Login", description = "Autentica o usuário e retorna um token JWT.")
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
       return ResponseEntity.ok(authService.login(data));
     }
 
+    @Operation(summary = "User Registration", description = "Registra um novo usuário no sistema.")
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
         if(userRepository.findByLogin(data.login()) != null){
